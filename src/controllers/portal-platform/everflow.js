3374ae5f6e674fae3ba5988905be67048eee5e86
sanjay <sanjay.sharma@zappian.com>
2024-06-26 16:48:31 +0530

    update everflow apis

--- src/controllers/portal-platform/everflow.js
+++ src/controllers/portal-platform/everflow.js
@@ -36,7 +36,8 @@ export async function storeEverflowAdvertisersDetails(req, res) {
 
 export async function getEverFlowAdvertiserList(req, res) {
     try {
-        const result = await findMany(EverFlowAdvertiserSchema, { account_status: "active" });
+        const networkId = req.body.data.id == '' ? { account_status: "active" } : { account_status: "active", network_id: req.body.data.id };
+        const result = await findMany(EverFlowAdvertiserSchema, networkId);
         const data = result.map(item => ({ label: item.name, value: item.network_advertiser_id }));
         res.success({ data: data });
     } catch (error) {
@@ -56,7 +57,8 @@ export async function storeEverflowOffersDetails(req, res) {
 
 export async function getEverFlowOfferList(req, res) {
     try {
-        const result = await findMany(EverFlowOfferSchema, { offer_status: "active" });
+        const offerId = req.body.data.id == '' ? { offer_status: "active" } : { offer_status: "active", network_advertiser_id: req.body.data.id };
+        const result = await findMany(EverFlowOfferSchema, offerId);
         const data = result.map(item => ({ label: item.name, value: item.network_offer_id }));
         res.success({ data: data });
     } catch (error) {
