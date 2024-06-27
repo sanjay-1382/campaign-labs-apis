a9396ba8bb2d9e81cea3214d0e31954c376112cd
manishzappi <manish.manker@zappian.com>
2024-06-27 15:45:10 +0530

    update header module

--- src/models/presets/header.js
+++ src/models/presets/header.js
@@ -4,18 +4,19 @@ const header = new Schema({
     headerName: { type: String, required: true, trim: true },
     headerMessage: { type: String, required: true },
     associtedId: { type: Number, default: null },
-    createdAt: { type: Date },
-    createdId: { type: Number },
-    createdBy: { type: String },
-    updatedAt: { type: Date },
-    updatedId: { type: Number },
-    updatedBy: { type: String },
-    deletedId: { type: Number },
-    deletedBy: { type: String },
+    createdAt: Date,
+    createdId: Number,
+    createdBy: String,
+    updatedAt: Date,
+    updatedId: Number,
+    updatedBy: String,
+    deletedAt: Date,
+    deletedId: Number,
+    deletedBy: String,
     isActive: { type: Boolean, default: true },
     isDeleted: { type: Boolean, default: false },
 }, {
-    timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' }
+    timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt', deletedAt: 'deletedAt' }
 });
 
 header.pre('save', async (next) => { next(); });
