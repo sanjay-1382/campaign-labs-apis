667648af728e4ee0cd21eaca9b07272db8389938
sanjay <sanjay.sharma@zappian.com>
2024-06-28 14:30:00 +0530

    code correction

--- src/controllers/presets/template.js
+++ src/controllers/presets/template.js
@@ -57,11 +57,11 @@ export const getAllTemplates = async (req, res) => {
             { fieldName: "Template Html", field: "templateHtml", filter: true },
             { fieldName: "Template Text", field: "templateText", filter: true },
             { fieldName: "Template Type", field: "templateType", filter: true },
-            { fieldName: "createdBy", field: "createdBy", filter: true },
-            { fieldName: "updatedBy", field: "updatedBy", filter: true },
-            { fieldName: "deletedBy", field: "deletedBy", filter: true },
-            { fieldName: "status", field: "isActive", filter: true },
-        ]
+            { fieldName: "Created By", field: "createdBy", filter: true },
+            { fieldName: "Updated By", field: "updatedBy", filter: true },
+            { fieldName: "Deleted By", field: "deletedBy", filter: true },
+            { fieldName: "Status", field: "isActive", filter: true },
+        ];
 
         return res.success({ data: { headers, data } });
     } catch (error) {
